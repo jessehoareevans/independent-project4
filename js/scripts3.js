80976b923d134bca22a90de729e5473c8d01b9e4
Jesse Hoare Evans <jesse.hoareevans@gmail.com>
2017-04-07 16:06:45 -0700

    add background color and three images to display

--- js/scripts3.js
+++ js/scripts3.js
@@ -12,14 +12,17 @@ Pizza.prototype.price = function() {
     pizzaPrice += 2;
     } else if ((this.toppings >=3) && (this.size === "Small")) {
     pizzaPrice += 4;
+
     } if ((this.toppings <=3) && (this.size === "Medium")) {
       pizzaPrice += 6;
     } else if ((this.toppings >=3) && (this.size === "Medium")) {
       pizzaPrice += 8;
+
     } if ((this.toppings <=3) && (this.size === "Large")) {
       pizzaPrice += 10;
     } else if ((this.toppings >=3) && (this.size === "Large")) {
       pizzaPrice += 12;
+
     } if ((this.toppings <=3) && (this.size === "Gigantic")) {
       pizzaPrice += 14;
     } else if ((this.toppings >=3) && (this.size === "Gigantic")) {
@@ -31,9 +34,6 @@ Pizza.prototype.price = function() {
 $(document).ready(function() {
   $("#pizza-info").submit(function(event) {
     event.preventDefault();
-    $("#price-output").show()
-    $('span#price-output');
-   });
 
     var inputtedToppings = $("#toppings").val();
     var inputtedSize = $("#size").val();
@@ -41,13 +41,15 @@ $(document).ready(function() {
 
     $("input:checkbox[name=toppings]:checked").each(function(){
       toppingsArray.push($(this).val());
+      var newPizzaPrice= new Pizza(inputtedToppings, inputtedSize, toppingsArray)
+
+      $("#output").text();
+      $("span#price-output").text(newPizzaPrice.price());
     });
-
-
-    var newPizzaPrice= new Pizza(inputtedToppings, inputtedSize, toppingsArray);
-
-    $("#output").show();
-    $("span#price-output").text(newPizzaPrice.price());
-
-
   });
+});
+
+
+
+    // $("#output").show();
+    // $("span#price-output").text(newPizzaPrice.price());
