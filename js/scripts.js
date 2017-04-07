081e750c1fb2e537d48eacfeba8d889a7a444fa5
Jesse Hoare Evans <jesse.hoareevans@gmail.com>
2017-04-07 08:32:51 -0700

    first js business logic added

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,24 @@ function Pizza(sauce, toppings, size, crust) {
   this.toppings = toppings;
   this.size = size;
 }
+
+Pizza.prototype.price = function() {
+  if (this.crust === "Hand Tossed" || "Thin Crust") {
+    pizzaPrice += 5;
+  } else if (this.crust === "Deep Dish") {
+    pizzaPrice += 7;
+  }
+  if (this.sauce === "Marinara" || "Barbecue" || "White Garlic") {
+    pizzaPrice += 2;
+  }
+  if (this.toppings === "Pepperoni" || "Chicken" || "Sausage" || "Mushrooms" || "Olives" || "Bell Peppers")
+    pizzaPrice += 1;
+    if (this.size === "Medium") {
+      pizzaPrice = 3;
+    } else if (this.size === "Large") {
+      pizzaPrice = 6;
+    } else if (this.size === "Gigantic") {
+      pizza.Price = 12;
+    }
+    return pizzaPrice
+}
